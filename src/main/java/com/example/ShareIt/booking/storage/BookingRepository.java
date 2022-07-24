36cc80e49e0750113cda8265d0c83f294ef05529
Deonis <deneshua@gmail.com>
2022-07-25 02:01:16 +0300

    update: check style

--- src/main/java/com/example/ShareIt/booking/storage/BookingRepository.java
+++ src/main/java/com/example/ShareIt/booking/storage/BookingRepository.java
@@ -6,9 +6,14 @@ import java.time.LocalDateTime;
 import java.util.Collection;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
+
     Collection<Booking> findBookingsByBookerIdOrderByStartDesc(long userId);
+
     Collection<Booking> findAllByItem_Owner_IdOrderByStartDesc(long userId);
+
     Collection<Booking> findBookingsByItemIdAndEndBeforeOrderByStartDesc(long userId, LocalDateTime now);
+
     Collection<Booking> findBookingsByItemIdAndStartAfterOrderByStartDesc(long userId, LocalDateTime now);
+
     Collection<Booking> findAllByBooker_IdAndItem_Id(long userId, long itemId);
 }
