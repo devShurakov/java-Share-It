86b3f7fb1e22dcd6f413c36cf09d96cfcdef0de8
Deonis <deneshua@gmail.com>
2022-08-28 17:56:15 +0300

    updated: убрана валидация из ItemController на сервере

--- server/src/main/java/ru/practicum/shareit/item/exception/ItemNotFoundException.java
+++ server/src/main/java/ru/practicum/shareit/item/exception/ItemNotFoundException.java
@@ -1,8 +1,4 @@
-<<<<<<< HEAD:server/src/main/java/ru/practicum/shareit/item/exception/ItemNotFoundException.java
 package ru.practicum.shareit.item.exception;
-=======
-package com.example.ShareIt.item.exception;
->>>>>>> main:src/main/java/com/example/ShareIt/item/exception/ItemNotFoundException.java
 
 public class ItemNotFoundException extends RuntimeException {
     public ItemNotFoundException(String message) {
